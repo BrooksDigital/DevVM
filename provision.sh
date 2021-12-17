120915f32e89417e12fbfc4927540b937622c5a5
Ariel Barreiro <abarrei@gmail.com>
2021-12-17 20:09:30 -0300

    Running updates

--- provision.sh
+++ provision.sh
@@ -3,9 +3,23 @@
 if [ ! -f /root/.provision/update ]; then
   echo "First time provisioning..."
   mkdir -p /root/.provision
-  bash /vagrant/provision/first.sh
+  /vagrant/provision/first.sh
   VERSION=$(ls -1 /vagrant/provision/updates/ | tail -1 | sed -e s/[^0-9]//g)
   echo $VERSION > /root/.provision/update
 fi
 
-bash /vagrant/provision/always.sh
+/vagrant/provision/always.sh
+
+if [ -f /root/.provision/update ]; then
+  VERSION=$(cat /root/.provision/update)
+  for f in $(ls -1 /vagrant/provision/updates/*.sh); do
+    FILENAME=${f##*/}
+    if [[ $FILENAME =~ ^update-([0-9]{4})\.sh$ ]]; then
+      if [[ "$FILENAME" > "update-$VERSION.sh" ]]; then
+        echo "Running update $f..."
+        $f
+        echo ${BASH_REMATCH[1]} > /root/.provision/update
+      fi
+    fi
+  done
+fi
