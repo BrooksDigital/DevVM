8bc51ca2c00244ee9b7c053a383e49ecf940bb0f
Ariel Barreiro <abarrei@gmail.com>
2022-02-11 00:41:37 -0300

    More verbose provisioning scripts

--- provision/always.sh
+++ provision/always.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Copy over specifc etc files
+echo "Copy over specifc etc files..."
 cp -R /vagrant/etc/* /etc
 
 # Keeping the VM up to date
+echo "Apt update ..."
 for i in 1 2 3 4 5; do apt-get update -y && break ; done
+echo "Apt upgrade..."
 for i in 1 2 3 4 5; do apt-get upgrade -y && break ; done
 
-# General ssh global config for vagrant user
+echo "General ssh global config for vagrant user..."
 cp -R /tmp/host/.ssh/* /home/vagrant/.ssh
 cp /vagrant/ssh/config /home/vagrant/.ssh/config
 chown -R vagrant:vagrant /home/vagrant/.ssh
 chmod 400 /home/vagrant/.ssh/config
 
-# vscode-server settings
+echo "vscode-server settings..."
 cp -R /vagrant/vscode-server /home/vagrant/.vscode-server
 chown -R vagrant:vagrant /home/vagrant/.vscode-server
--- provision/first.sh
+++ provision/first.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# Copy over specifc etc files
+echo "Copy over specifc etc files..."
 cp -R /vagrant/etc/* /etc
 
-# Enable utf en_US locale
+echo "Enable utf en_US locale..."
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-# Restart/apply config files copied above
+echo "Restart/apply config files copied above..."
 sysctl --system
 
-# General tools
+echo "Apt update..."
 for i in 1 2 3 4 5; do apt-get update -y && break ; done
+echo "Apt install general tools..."
 for i in 1 2 3 4 5; do apt-get install -y git jq htop net-tools && break ; done
 
 # Docker setup based of https://docs.docker.com/engine/install/debian/
+echo "Apt install more tools..."
 for i in 1 2 3 4 5; do apt-get install -y ca-certificates curl gnupg lsb-release && break ; done
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "Apt update..."
 for i in 1 2 3 4 5; do apt-get update -y && break ; done
+echo "Apt install docker..."
 for i in 1 2 3 4 5; do apt-get install -y docker-ce docker-ce-cli containerd.io && break ; done
 
-# Add vagrant to the docker group so it can be used by the user
+echo "Add vagrant to the docker group so it can be used by the user..."
 usermod -a -G docker vagrant
 
-# lando
+echo "Install lando..."
 LANDO_VERSION=3.6.0
 wget -q https://github.com/lando/lando/releases/download/v$LANDO_VERSION/lando-x64-v$LANDO_VERSION.deb
 dpkg -i lando-x64-v$LANDO_VERSION.deb
